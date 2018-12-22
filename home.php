5932a782e9aa9cc2b047ba7505a11133a9a7c2db
MunDurf <46076701+MunDurf@users.noreply.github.com>
2018-12-23 00:18:10 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<link rel="shortcut icon " href="../Images/FondMunDurf2.PNG" type="image/x-icon"/>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="steelsheet.css"> 
+	<title>Home</title>
+</head>
+<body>
+	<section class="vitrine">
+		<h1>Nos clients:</h1>
+	
+		<div id="siteClient">
+			<div id="pdp" style="background-image: url(../Images/Audric.jpg);background-size: cover;"></div>
+			<div id="descriptif" >
+				<h2>Audric</h2>
+				<p>MoonDurf le beau gosse de la mort qui tue même si la mort tue forcément.. ouais,c'est logique .</p>
+			</div>
+		</div>
+
+		<div id="siteClient">
+			<div id="pdp" style="background-image: url(../Images/xD.jpg);background-size: cover;"></div>
+			<div id="descriptif">
+				<h2>4trous</h2>
+				<p>MoonDurf le beau gosse de la mort qui tue même si la mort tue forcément.. ouais,c'est logique .</p>
+			</div>
+		</div>	
+		
+		<div id="siteClient">
+			<div id="pdp" style="background-image: url(../Images/madjoe.PNG);background-size: cover;"></div>
+			<div id="descriptif">
+				<h2>MadJoe</h2>
+				<p>MoonDurf le beau gosse de la mort qui tue même si la mort tue forcément.. ouais,c'est logique .</p>
+			</div>
+		</div>
+
+		<div id="siteClient">
+			<div id="pdp" style="background-image: url(../Images/jdp.jpg);background-size: cover;"></div>
+			<div id="descriptif">
+				<h2>5tinfol</h2>
+				<p>MoonDurf le beau gosse de la mort qui tue même si la mort tue forcément.. ouais,c'est logique .</p>
+			</div>
+		</div>			
+
+	</section>
+</body>
+</html>
